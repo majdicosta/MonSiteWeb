f3536e9b954083b8fc2e54d60b5d83bc8ade9595
majdicosta <bennaouimajdi@gmail.com>
2014-11-11 20:07:30 +0100

    first commit

---
+++ core/config.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * information sur la base des données
+ */
+
+define('DB_TYPE','mysql' );
+define('DB_HOST','localhost' );
+define('DB_NAME','MonSiteWeb' );
+define('DB_USER','root' );
+define('DB_PASSWORD','' );
+
+/**
+ * RedBean Configuration
+ */
+define('REDBEAN_MODEL_PREFIX','\\MonSiteWeb\\Model');
